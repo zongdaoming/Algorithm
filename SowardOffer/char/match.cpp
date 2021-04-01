9e2468882afd316c45107347ab0bb716fafec68d
dorming <m15901768536@163.com>
2021-04-02 00:20:15 +0800

    char_change

--- SowardOffer/char/match.cpp
+++ SowardOffer/char/match.cpp
@@ -10,6 +10,7 @@
 #include <algorithm>
 using namespace std;
 
+// 递归的套路
 
 class Solution {
 public:
@@ -21,13 +22,30 @@ public:
      * @param pattern string字符串 
      * @return bool布尔型
      */
+    bool matchCore(string &str, string &pattern, int i, int j)
+    {
+        //递归边界设置
+        if(i==str.size() && j==pattern.size()) return true;
+        if(i!=str.size() && j==pattern.size()) return false;
+        
+        if (pattern[j+1]=='*')
+        {
+            if(str[i]==pattern[j] || (pattern[j]=='.' && i!=str.size()))
+            {
+                return matchCore(str, pattern, i+1, j) || matchCore(str, pattern, i+1, j+2) || matchCore(str,pattern, i, j+2);
+            }
+            else return matchCore(str, pattern, i, j+2);
+        }
+        
+        if(str[i]==pattern[j] || (pattern[j]=='.' && i!=str.size()))
+        {
+            return matchCore(str, pattern, i+1, j+1);
+        }
+        return false;
+    }
     bool match(string str, string pattern) {
         // write code here
-        
-
-
-        
-        
-        
+        //递归
+        return matchCore(str, pattern,0,0);
     }
 };
