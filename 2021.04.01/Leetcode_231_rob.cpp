6bec21084784479cb2eb3f5a0119d911db2615b4
dorming <m15901768536@163.com>
2021-04-16 23:58:27 +0800

    skills

--- 2021.04.01/Leetcode_231_rob.cpp
+++ 2021.04.01/Leetcode_231_rob.cpp
@@ -16,6 +16,7 @@ class Solution {
 public:
     int robRange(vector<int>& nums, int start, int end) {
         // dp的边界条件
+        // 滚动数组的技巧
         int first = nums[start], second = max(nums[start], nums[start + 1]);
         for (int i = start + 2; i <= end; i++) {
             int temp = second;
