c940ff35ce4871388a6b1c965daf3f3e311fb2f7
dorming <m15901768536@163.com>
2021-04-16 23:57:21 +0800

    rob

--- 2021.04.01/Leetcode_231_rob.cpp
+++ 2021.04.01/Leetcode_231_rob.cpp
@@ -15,6 +15,7 @@ using namespace std;
 class Solution {
 public:
     int robRange(vector<int>& nums, int start, int end) {
+        // dp的边界条件
         int first = nums[start], second = max(nums[start], nums[start + 1]);
         for (int i = start + 2; i <= end; i++) {
             int temp = second;
@@ -26,6 +27,9 @@ public:
 
     int rob(vector<int>& nums) {
         int length = nums.size();
+        // 确定边界条件
+        // 只有一间房屋，则偷窃该房屋
+        // 只有两间房屋，偷窃其中金额较高的房屋
         if (length == 1) {
             return nums[0];
         } else if (length == 2) {
