6bc87dedc99766c2613dfefe319883fe13d9899a
dorming <m15901768536@163.com>
2021-05-09 23:11:19 +0800

    high expression

--- 2021.04.19/Leetcode_10_isMatch.cpp
+++ 2021.04.19/Leetcode_10_isMatch.cpp
@@ -12,40 +12,41 @@
 using namespace std;
 
 
-// 其中 dp[i][j] 表示 以i 戒止的字符串是否可以被j截止的怎则表达式匹配
-// 其中 dp[i][j] 表示以 i 截止的字符串是否可以被以 j 截止的 正则表达式匹配。
+// 我们用 f[i][j] 表示 s 的前 i个字符与 p 中的前 j 个字符是否能够匹配
+
 class Solution {
 public:
-bool isMatch(string s, string p) { 
-    int m = s.size(), n = p.size(); 
-    vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false)); 
-    dp[0][0] = true; 
-    for (int i = 1; i < n + 1; ++i) 
-    { 
-        if (p[i-1] == '*') { 
-        dp[0][i] = dp[0][i-2];
-        } 
-    }
-    for (int i = 1; i < m + 1; ++i) 
-    { 
-        for (int j = 1; j < n + 1; ++j) { 
-        if (p[j-1] == '.') { 
-            dp[i][j] = dp[i-1][j-1];
-        } 
-        else if (p[j-1] != '*') { 
-            dp[i][j] = dp[i-1][j-1] && p[j-1] == s[i-1];
-            } 
-        else if (p[j-2] != s[i-1] && p[j-2] != '.') 
-        { 
-            dp[i][j] = dp[i][j-2];
-        } 
-        else 
-        { 
-            dp[i][j] = dp[i][j-1] || dp[i-1][j] || dp[i][j-2];     
-        } 
-        } 
-    } 
-    return dp[m][n]; 
-}
-};
+    bool isMatch(string s, string p) {
+        int m = s.size();
+        int n = p.size();
 
+        auto matches = [&](int i, int j) {
+            if (i == 0) {
+                return false;
+            }
+            if (p[j - 1] == '.') {
+                return true;
+            }
+            return s[i - 1] == p[j - 1];
+        };
+
+        vector<vector<int>> f(m + 1, vector<int>(n + 1));
+        f[0][0] = true;
+        for (int i = 0; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (p[j - 1] == '*') {
+                    f[i][j] |= f[i][j - 2];
+                    if (matches(i, j - 1)) {
+                        f[i][j] |= f[i - 1][j];
+                    }
+                }
+                else {
+                    if (matches(i, j)) {
+                        f[i][j] |= f[i - 1][j - 1];
+                    }
+                }
+            }
+        }
+        return f[m][n];
+    }
+};
